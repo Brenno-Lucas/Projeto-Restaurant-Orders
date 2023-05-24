8d8177cc49ead85e80f3d91c81c4380d166c10e3
Brenno-Lucas <brennolucas8@gmail.com>
2023-05-24 09:14:14 -0300

    Requisito 02

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,27 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    with pytest.raises(TypeError, match="Dish price must be float."):
+        Dish("bigmac", "43")
+    with pytest.raises(
+        ValueError, match="Dish price must be greater then zero."
+    ):
+        Dish("bigmac", -43)
+    test_1 = Dish("lanche", 25.00)
+    test_2 = Dish("pizza", 60.00)
+    assert test_1.name == "lanche"
+    assert test_1.__hash__() != test_2.__hash__()
+    assert test_1.__hash__() == test_1.__hash__()
+    assert (test_1 == test_1) is True
+    assert (test_1 == test_2) is False
+    assert test_1.__repr__() == "Dish('lanche', R$25.00)"
+    test_1.add_ingredient_dependency(Ingredient("carne"), 3)
+    assert test_1.get_ingredients() == {Ingredient('carne')}
+    assert test_1.get_restrictions() == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
